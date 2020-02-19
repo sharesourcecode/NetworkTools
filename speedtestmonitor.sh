b54ab9ea7219140a51ecbb941011cb63fc1dded3
Ton Guitar <49400447+sharesourcecode@users.noreply.github.com>
2020-02-19 13:46:23 -0300

    Add files via upload

--- speedtestmonitor.sh
+++ speedtestmonitor.sh
@@ -2,30 +2,31 @@
 termux-wake-lock &> /dev/null
 if [[ $? = 0 ]]; then
 	termux-setup-storage
-	STORAGE=$HOME/storage/downloads/
-	echo -e "Result will be saved in /storage/emulated/0/Downloads/tests.txt\n"
+	STORAGE=$HOME/storage/downloads/speedtestmonitor/
 else
-	STORAGE=$HOME
-	echo -e "Result will be saved in $HOME/tests.txt\n"
+	STORAGE=$HOME/speedtestmonitor/
 fi
+mkdir -p $STORAGE
 cd $STORAGE
-FILE=tests.txt
-if [[ -n $FILE ]]; then
+FILE="tests_$(date +%d_%m_%Y).txt"
+echo -e "Result will be saved in:\n`pwd`\n$FILE\n"
+if [[ -e $FILE && -n $FILE ]]; then
 	read -p "File not empty. Overwrite? [y/n] " -e -n 1 OP
 	case $OP in
-		(y|Y)
-		clear; >tests.txt; echo 'Starting...' ;;
+		(y|Y) clear; >$FILE; echo 'Starting...' ;;
 		(n|N) clear; echo 'Starting...' ;;
 		(*) echo -e "nInvalid optionn"; exit 0 ;;
 	esac
+else
+	touch $FILE
 fi
 while true; do
-echo -e "\n" >> tests.txt
-echo 'https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python -' >>tests.txt
-curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python - >>tests.txt
-date +%H:%M:%S_%d/%m/%Y >> tests.txt
-echo -e "\n" >> tests.txt
-cat tests.txt | tail -n9
+echo -e "\n" >>$FILE
+echo 'https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python -' >>$FILE
+curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python - >>$FILE
+date +%H:%M:%S_%d/%m/%Y >>$FILE
+echo -e "\n" >>$FILE
+cat $FILE | grep ':' | tail -n4
 sleep 30m
 done
 exit
