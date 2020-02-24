e094a066daa14213ebc9a1237c147f22c7b74b3f
Ton Guitar <49400447+sharesourcecode@users.noreply.github.com>
2020-02-24 10:16:04 -0300

    Add files via upload

---
+++ bettermtu.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo 'Long tests will be done, be patient.'
+num=1493
+RCVDD=""
+until [[ -n $RCVD && $RCVD -eq 10 || $num -lt 576 ]]; do
+	num=$[$num-1]
+	echo -e "Checking $num bytes..."
+	PING=$(ping -s $num -c10 google.com)
+	RCVD=$(echo $PING | grep -o '10 received' | tr -cd "[[:digit:]]")
+	clear
+done
+if [[ $RCVD -eq 10 ]]; then
+	echo -e "\n MTU: $(echo $PING | grep 'PING' | cut -d\) -f2 | cut -d\( -f2)"
+else
+	echo -e "\nA connection error has occurred, MTU cannot be found. Please check your network and try again."
+fi
+exit
